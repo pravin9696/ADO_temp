166a0d97d8a89867c60ec136334abf18827134ee
pravin gaikwad <pravingaikwad.sibar@outlook.com>
2024-03-15 15:49:27 +0530

    CRUD operation

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,71 +35,156 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(81, 40);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 37);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.Size = new System.Drawing.Size(35, 24);
             this.label1.TabIndex = 0;
             this.label1.Text = "roll";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(81, 89);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(27, 86);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(61, 24);
             this.label2.TabIndex = 1;
             this.label2.Text = "Name";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(81, 137);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(27, 134);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.Size = new System.Drawing.Size(70, 24);
             this.label3.TabIndex = 2;
             this.label3.Text = "contact";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(177, 40);
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(149, 34);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(277, 29);
             this.textBox1.TabIndex = 3;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(179, 86);
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(149, 83);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.Size = new System.Drawing.Size(277, 29);
             this.textBox2.TabIndex = 4;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(179, 134);
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(149, 131);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.Size = new System.Drawing.Size(277, 29);
             this.textBox3.TabIndex = 5;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(41, 229);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(22, 219);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(90, 47);
             this.button1.TabIndex = 6;
             this.button1.Text = "Search";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(22, 311);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 47);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "insert";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(268, 205);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(124, 61);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Insert Stud Record";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(430, 205);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(124, 61);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Insert using SP";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(268, 286);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(124, 61);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Update Record";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(430, 286);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(124, 61);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Delete Record";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(268, 377);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(124, 61);
+            this.button7.TabIndex = 12;
+            this.button7.Text = "Search Student";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
@@ -123,6 +208,12 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -36,6 +38,177 @@ namespace ADO_temp
             {
                 MessageBox.Show("record not found");
                 // this is new line added from github
+            }
+            con.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection();
+            SqlTransaction transaction;
+            con.ConnectionString = "Data Source=.\\SQLExpress;Initial Catalog=DB_130324_ADO;Integrated Security=True;Encrypt=True;TrustServerCertificate=True";
+            con.Open();
+            transaction = con.BeginTransaction();
+            try
+            {          
+           
+            String myQuery = "insert into tblStudent values("+textBox1.Text+",'"+textBox2.Text+"',"+textBox3.Text+")";
+            SqlCommand cmd1 = new SqlCommand(myQuery, con,transaction);
+                SqlCommand cmd2 = new SqlCommand("insert into temp values("+textBox1.Text+",'"+textBox2.Text+"')", con,transaction);
+                int m = cmd2.ExecuteNonQuery();
+                int n=cmd1.ExecuteNonQuery();
+               
+                if (n>0)
+            {
+                MessageBox.Show("Record inserted successfully");
+            }
+            else
+            {
+                MessageBox.Show("Record not inserted!!!!");
+            }
+
+              transaction.Commit();
+                MessageBox.Show("commit done..");
+            }
+            catch (SqlException se)
+            {
+               
+                transaction.Rollback();
+                MessageBox.Show("Roll back done..");
+
+            }
+
+
+            con.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //step1 
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "Data Source=.\\sqlexpress;Initial Catalog=DB_130324_ADO;Integrated Security=True;TrustServerCertificate=True";
+            con.Open();
+
+            //step2
+            int roll = int.Parse(textBox1.Text);
+            string name = textBox2.Text;
+            int contact = int.Parse(textBox3.Text);
+
+             string qry= "insert into tblStudent values("+roll+",'"+name+"',"+contact+")";
+
+            // SqlCommand cmd = new SqlCommand(qry, con);
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = qry;
+            cmd.CommandType = CommandType.Text;
+
+            //step3
+            int n=cmd.ExecuteNonQuery();
+            if (n<=0)
+            {
+                MessageBox.Show("Record not inserted !!!");
+            }
+            else
+            {
+                MessageBox.Show("Record inserted Successfully.....");
+
+            }
+
+            con.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //step1 
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "Data Source=.\\sqlexpress;Initial Catalog=DB_130324_ADO;Integrated Security=True;TrustServerCertificate=True";
+            con.Open();
+
+            //step2
+            int roll = int.Parse(textBox1.Text);
+            string name = textBox2.Text;
+            int contact = int.Parse(textBox3.Text);
+
+            string qry = "sp_insetStud";
+
+            // SqlCommand cmd = new SqlCommand(qry, con);
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = qry;
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@roll", roll);
+            cmd.Parameters.AddWithValue("@name", name);
+            cmd.Parameters.AddWithValue("@contact", contact);
+
+            //step3
+            int n = cmd.ExecuteNonQuery();
+            if (n <= 0)
+            {
+                MessageBox.Show("Record not inserted !!!");
+            }
+            else
+            {
+                MessageBox.Show("Record inserted Successfully.....");
+
+            }
+
+            con.Close();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=.\\sqlexpress;Initial Catalog=DB_130324_ADO;Integrated Security=True;TrustServerCertificate=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("update tblStudent set name='" + textBox2.Text + "',contact=" + textBox3.Text + " where roll=" + textBox1.Text,con);
+
+            int n=cmd.ExecuteNonQuery();
+            if (n>0)
+            {
+                MessageBox.Show("Record updated successfully..");
+            }
+            else
+
+            {
+                MessageBox.Show("Record not updated!!!!");
+            }
+            con.Close();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=.\\sqlexpress;Initial Catalog=DB_130324_ADO;Integrated Security=True;TrustServerCertificate=True");
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("delete from tblStudent where roll=" + textBox1.Text, con);
+              int n=cmd.ExecuteNonQuery();
+            if (n>0)
+            {
+                MessageBox.Show("Record deleted successfully....");
+            }
+            else
+            {
+                MessageBox.Show("Record NOT deleted");
+            }
+            con.Close();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=.\\sqlexpress;Initial Catalog=DB_130324_ADO;Integrated Security=True;TrustServerCertificate=True");
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("select * from tblStudent where roll=" + textBox1.Text, con);
+            
+
+            SqlDataReader rdr= cmd.ExecuteReader();
+            if (rdr.Read())
+            {
+                textBox2.Text = rdr[1].ToString();//   0-> roll  1-> name   2-> contact
+                textBox3.Text = rdr["contact"].ToString();
+            }
+            else
+            {
+                MessageBox.Show("Record not found!!!");
             }
             con.Close();
         }
